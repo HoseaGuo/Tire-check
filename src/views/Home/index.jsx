ea406c5da7478bc69c41e4462940a4c9f64947b4
guohaoxian <guohx2010@163.com>
2023-09-05 18:32:47 +0800

    update

--- src/views/Home/index.jsx
+++ src/views/Home/index.jsx
@@ -8,6 +8,7 @@ import 'swiper/css/navigation';
 
 import './index.scss';
 import Circle from '../../components/Circle';
+import { useState } from 'react';
 
 const columns = [
   {
@@ -34,6 +35,8 @@ const columns = [
 ];
 
 function CheckView() {
+  let [previewVisible, setPreviewVisible] = useState(false);
+
   let points = [
     {
       angle: 0,
@@ -100,7 +103,7 @@ function CheckView() {
       </div> */}
       <div className="bottom">
         <Table columns={columns} pagination={false} dataSource={data} size="small" style={{ width: '49%' }} scroll={{ y: '40vh' }} />
-        <div className="information flex">
+        <div className="information flex" onClick={() => setPreviewVisible(true)}>
           <div className="flex-1">
             <p>轮圈条码</p>
             <p>轮圈型号</p>
@@ -112,40 +115,47 @@ function CheckView() {
           </div>
         </div>
       </div>
-      <div className="preview-image">
-        <button className="btn-close">
-          <svg t="1693909250861" viewBox="0 0 1024 1024" version="1.1" xmlns="http://www.w3.org/2000/svg" p-id="3985" width="48" height="48">
-            <path
-              d="M571.733333 512l268.8-268.8c17.066667-17.066667 17.066667-42.666667 0-59.733333-17.066667-17.066667-42.666667-17.066667-59.733333 0L512 452.266667 243.2 183.466667c-17.066667-17.066667-42.666667-17.066667-59.733333 0-17.066667 17.066667-17.066667 42.666667 0 59.733333L452.266667 512 183.466667 780.8c-17.066667 17.066667-17.066667 42.666667 0 59.733333 8.533333 8.533333 19.2 12.8 29.866666 12.8s21.333333-4.266667 29.866667-12.8L512 571.733333l268.8 268.8c8.533333 8.533333 19.2 12.8 29.866667 12.8s21.333333-4.266667 29.866666-12.8c17.066667-17.066667 17.066667-42.666667 0-59.733333L571.733333 512z"
-              fill="#ffffff"
-              p-id="3986"
-            ></path>
-          </svg>
-        </button>
-        <Swiper
-          className="swiper"
-          // onSlideChange={() => console.log('slide change')}
-          // onSwiper={(swiper) => console.log(swiper)}
-          navigation={true}
-          modules={[Navigation, Pagination]}
-          pagination={{
-            type: 'fraction',
-          }}
-        >
-          {new Array(5).fill(undefined).map((item, index) => {
-            return (
-              <SwiperSlide key={index}>
-                <div className="preview-item">
-                  <p className="detail">正面：50°</p>
-                  <div className="img-box">
-                    <img src="https://img0.baidu.com/it/u=2528674402,2721043688&fm=253&fmt=auto&app=138&f=JPEG?w=500&h=330" alt="" />
+      {previewVisible && (
+        <div className="preview-image">
+          <button
+            className="btn-close"
+            onClick={() => {
+              setPreviewVisible(false);
+            }}
+          >
+            <svg t="1693909250861" viewBox="0 0 1024 1024" version="1.1" xmlns="http://www.w3.org/2000/svg" p-id="3985" width="40" height="40">
+              <path
+                d="M571.733333 512l268.8-268.8c17.066667-17.066667 17.066667-42.666667 0-59.733333-17.066667-17.066667-42.666667-17.066667-59.733333 0L512 452.266667 243.2 183.466667c-17.066667-17.066667-42.666667-17.066667-59.733333 0-17.066667 17.066667-17.066667 42.666667 0 59.733333L452.266667 512 183.466667 780.8c-17.066667 17.066667-17.066667 42.666667 0 59.733333 8.533333 8.533333 19.2 12.8 29.866666 12.8s21.333333-4.266667 29.866667-12.8L512 571.733333l268.8 268.8c8.533333 8.533333 19.2 12.8 29.866667 12.8s21.333333-4.266667 29.866666-12.8c17.066667-17.066667 17.066667-42.666667 0-59.733333L571.733333 512z"
+                fill="#ffffff"
+                p-id="3986"
+              ></path>
+            </svg>
+          </button>
+          <Swiper
+            className="swiper"
+            // onSlideChange={() => console.log('slide change')}
+            // onSwiper={(swiper) => console.log(swiper)}
+            navigation={true}
+            modules={[Navigation, Pagination]}
+            pagination={{
+              type: 'fraction',
+            }}
+          >
+            {new Array(5).fill(undefined).map((item, index) => {
+              return (
+                <SwiperSlide key={index}>
+                  <div className="preview-item">
+                    <p className="detail">正面：50°</p>
+                    <div className="img-box">
+                      <img src="https://img0.baidu.com/it/u=2528674402,2721043688&fm=253&fmt=auto&app=138&f=JPEG?w=500&h=330" alt="" />
+                    </div>
                   </div>
-                </div>
-              </SwiperSlide>
-            );
-          })}
-        </Swiper>
-      </div>
+                </SwiperSlide>
+              );
+            })}
+          </Swiper>
+        </div>
+      )}
     </div>
   );
 }
