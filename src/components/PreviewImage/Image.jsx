c378fe26ab0f13bb16c082b33f0a8b56eb78e2f0
guohaoxian <guohx2010@163.com>
2023-09-14 00:25:09 +0800

    '图片不存在情况提示优化'

--- src/components/PreviewImage/Image.jsx
+++ src/components/PreviewImage/Image.jsx
@@ -4,17 +4,21 @@ import { useEffect } from 'react';
 
 export default function ({ imagePath, init, qrCode }) {
   let [imageData, setImageData] = useState("");
+  let [errMsg, setErrMsg] = useState("");
 
   async function loadImage() {
     let imageData = imageDataMap.get(imagePath);
     if (imageData) {
       setImageData(imageData)
     } else {
-      let { success, data } = await api.getImage({ imagePath, qrCode });
+      let { success, data, msg } = await api.getImage({ imagePath, qrCode });
       if (success) {
         imageData = `data:image/webp;base64,${data.base64}`;
         imageDataMap.set(imagePath, imageData);
         setImageData(imageData)
+        setErrMsg("");
+      } else {
+        setErrMsg(msg);
       }
     }
   }
@@ -24,7 +28,12 @@ export default function ({ imagePath, init, qrCode }) {
       loadImage();
     }
   }, [init])
-
+  if (errMsg) return <div style={{ width: '200px', textAlign: 'center' }}>
+    <p><span style={{ color: "#fff" }}>{errMsg}</span></p>
+    {
+      errMsg !== '文件不存在' && <button onClick={loadImage} style={{ padding: '6px 10px', marginTop: '30px' }}>点击重新加载</button>
+    }
+  </div>
   return !imageData ? <div>加载中...</div> : <img src={imageData} />;
 }
 
